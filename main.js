d9ec00b5eacd4297533a19bbf9755d1968f9607b
Kristian Rokholm <kristian_rokholm@live.dk>
2017-02-27 15:37:37 +0100

    Google stuff 2.0

--- main.js
+++ main.js
@@ -1,46 +1,8 @@
-<<<<<<< HEAD
 
 
-function GoogleSignIn(){
-var provider = new firebase.auth.GoogleAuthProvider();
-
-/* 
-2.Optional: Specify additional OAuth 2.0 scopes that you want to request from the authentication provider. To add a scope, call addScope. For example:
-provider.addScope('https://www.googleapis.com/auth/plus.login');
-See the authentication provider documentation.
-
-3.Optional: Specify additional custom OAuth provider parameters that you want to send with the OAuth request. To add a custom parameter, call setCustomParameters on the initialized provider with an object containing the key as specified by the OAuth provider documentation and the corresponding value. For example:
-provider.setCustomParameters({
-  'login_hint': 'user@example.com'
-});
-*/
+// If a function is named the same as another, when commit push the newest will overwrite the other.
 
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});}
-
-
-function GoogleSignOff(){
-firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-}, function(error) {
-  // An error happened.
-});}
-=======
 var config = {
     apiKey: "AIzaSyBa6c18Ip2mptMgiOUYuWgw3yTg-cm5-8A",
     authDomain: "mp-moive.firebaseapp.com",
@@ -50,44 +12,5 @@ var config = {
 };
 firebase.initializeApp(config);
 
+//==========================================================
 
-function facebookLogin() {
-
-    var provider = new firebase.auth.FacebookAuthProvider();
-
-    /*
-    Specify additional OAuth 2.0 scopes that you want to request from the authentication provider.
-    To add a scope, call addScope. For example:
-    provider.addScope('user_birthday');
-    */
-
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-        console.log(user.displayName);
-        document.getElementById("user").innerHTML = user.displayName;
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
-
-}
-
-function facebookSignOut() {
-    firebase.auth().signOut().then(function () {
-        // Sign-out successful.
-    }, function (error) {
-        // An error happened.
-    });
-
-}
->>>>>>> 71181ca579d0d01cde6787f9ea333940231ecdbf
