bb3e9a5b0b0dc2dfd08bc4a9518c2638f34d6ad6
Anders Viborg <viborg92@hotmail.com>
2017-03-05 02:08:02 +0100

    Displaying imdbRating + commenting

--- anders.js
+++ anders.js
@@ -44,11 +44,16 @@ function facebookSignOut() {
 
 function omdbSearch(){
     var input = document.getElementById('search').value;
-    var kek = 'https://www.omdbapi.com/?t=' +  encodeURI(input);
-    console.log(kek);
-    $.getJSON(kek).then(function(output){
+    //Setting the input we get from the search bar which is located in index.html.
+    var combinput = 'https://www.omdbapi.com/?t=' +  encodeURI(input);
+    //Creating a new varraible which is the combination of the input and the API for OMDB
+    console.log(combinput);
+    $.getJSON(combinput).then(function(output){
+        //Using jquery to gain the information from the api with the combined adress and input
         console.log(output);
         document.getElementById('plot').innerHTML = output.Plot;
+        document.getElementById('imdbRating').innerHTML = output.imdbRating;
+        //Using a div from index.html to display the plot from the object search on.
     });
 }
 
