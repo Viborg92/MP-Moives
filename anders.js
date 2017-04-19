82b6865f0729020ecbce44d5db03d6833a725647
Anders Viborg <viborg92@hotmail.com>
2017-04-19 15:40:03 +0200

    Trailers semi works

--- anders.js
+++ anders.js
@@ -40,7 +40,7 @@ function facebookSignOut() {
 function omdbSearch(input) {
     //   var input = document.getElementById('search').value;
     //Setting the input we get from the search bar which is located in index.html.
-    var combinput = 'https://www.omdbapi.com/?t=' + encodeURI(input) + '&tomatoes=true';
+    var combinput = 'https://www.omdbapi.com/?t=' + encodeURI(input) + '&plot=full';
     //Creating a new varraible which is the combination of the input and the API for OMDB
     console.log(combinput);
     $.getJSON(combinput).then(function (output) {
@@ -50,8 +50,30 @@ function omdbSearch(input) {
         document.getElementById('imdbRating').innerHTML = output.Ratings[0].Value;
         document.getElementById('tomatoRating').innerHTML = output.Ratings[1].Value;
         document.getElementById('Poster').innerHTML = '<img src="' + output.Poster + '" />';
+        //Using the YQL to scape imdb, using the ID from omdb.
+        $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.imdb.com%2Ftitle%2F" + output.imdbID + "%2F'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").then(function (trailerput){
+            console.log(trailerput);
+            //Here we call the function with the place we know the trailer ID is.
+            console.log(getObjects(trailerput, 'class', 'slate'));
+            var trailerSearch = "https://www.imdb.com/video/imdb/" + getObjects(trailerput, 'class', 'slate')[0].a["data-video"] + "/imdb/embed?autoplay=false&width=480";
+            document.getElementById('trailerFrame').src = trailerSearch;
+        });
         //Using a div from index.html to display the plot from the object search on.
     });
+}
+//Loops through the json file to find the class - slate - and then since java does not like "-" we need to to specify it as "[0].a["data-video"]" instead of
+//a.datavideo if there was no "-"
+function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else if (i == key && obj[key] == val) {
+            objects.push(obj);
+        }
+    }
+    return objects;
 }
 
 //When the document is done rendering, 
