b61fa9fa37334bbac84faa679f4c3f1cf4c1b64e
Kristian Rokholm <kristian_rokholm@live.dk>
2017-05-01 14:19:21 +0200

    Done some changes that should make the nearbySearches work. They dont.

--- nearbySearch.js
+++ nearbySearch.js
@@ -3,17 +3,21 @@ var service;
 var infowindow;
 
 function initialize() {
-  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+  //var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+ var yourPos = new google.maps.LatLng(pos);
+  
 
   map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
+   //center: pyrmont,
+      center: yourPos,
       zoom: 15
     });
 
   var request = {
-    location: pyrmont,
-    radius: '500',
-    types: ['store']
+    //location: pyrmont,
+    location: yourPos,
+    radius: '5000',
+    types: ['movie_theater']
   };
 
   service = new google.maps.places.PlacesService(map);
@@ -27,4 +31,18 @@ function callback(results, status) {
       createMarker(results[i]);
     }
   }
+
+ function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
+
 }
